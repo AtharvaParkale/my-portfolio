1d99b2bbb449e5443eed72a2cf1586c4d8c3b668
Atharva Parkale <atharva.parkale@gmail.com>
2022-12-26 21:02:06 +0530

    Contact me page completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,52 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import './Footer.css'
-import { FaFacebook } from 'react-icons/fa';
-import { AiFillTwitterSquare } from 'react-icons/ai';
-import { AiFillLinkedin } from 'react-icons/ai';
-import { FaInstagramSquare } from 'react-icons/fa';
-import { HiArrowCircleUp } from 'react-icons/hi';
-import { BsYoutube } from 'react-icons/bs';
-import footerLogo from '../../assets/images/businusDashboardLogo.7b4efcab.png';
+import React, { useState, useEffect } from "react";
+import "./Footer.css";
+import { HiArrowCircleUp } from "react-icons/hi";
 import { Link } from "react-scroll";
-import footer1 from '../../assets/images/footr1.png'
-import footer2 from '../../assets/images/footer2.png'
-import footer3 from '../../assets/images/footer3.png'
-import footer4 from '../../assets/images/footer4.png'
-import { Link as LinkPage } from 'react-router-dom';
-
-
 
 function Footer() {
-    const [footerArrow, setFooterArrow] = useState(false);
+  const [footerArrow, setFooterArrow] = useState(false);
 
-    const setArrow = () => {
-        console.log(window.scrollY);
-        if (window.scrollY < 110) {
-            setFooterArrow(false);
-        }
-        else {
-            setFooterArrow(true);
-        }
+  const setArrow = () => {
+    console.log(window.scrollY);
+    if (window.scrollY < 110) {
+      setFooterArrow(false);
+    } else {
+      setFooterArrow(true);
     }
-    useEffect(() => {
-        setArrow();
-        window.addEventListener("scroll", setArrow)
-    })
+  };
+  useEffect(() => {
+    setArrow();
+    window.addEventListener("scroll", setArrow);
+  });
 
-
-    return (
-        <>
-            <div className='footer_container'>
-                <span>Made with ❤️ by Atharva Parkale</span>
-            </div>
-            {footerArrow ? <div className="scroll_up">
-                <Link to="section-one" smooth={true} duration={900} >
-                    <HiArrowCircleUp size={60} />
-                </Link>
-            </div> : ""
-            }
-        </>
-    )
+  return (
+    <>
+      <div className="footer_container">
+        <span>Made with ❤️ by Atharva Parkale</span>
+      </div>
+      {footerArrow ? (
+        <div className="scroll_up">
+          <Link to="section-one" smooth={true} duration={900}>
+            <HiArrowCircleUp size={60} />
+          </Link>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/SectionEight/SectionEight.jsx
+++ src/Components/SectionEight/SectionEight.jsx
@@ -1,14 +1,114 @@
-import React from 'react'
-import './SectionEight.css'
-import familyImage from '../../assets/images/family_image.jpg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./SectionEight.css";
+import { EmailOutlined, PhoneOutlined } from "@mui/icons-material";
+
+import { BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
 
 function SectionEight() {
-    return (
-        <div className='sectionEight_container' id="section-eight">
-            
+  return (
+    <div className="sectionEight_container" id="section-eight">
+      <div className="sec-eight-header">
+        <h1>Contact Me</h1>
+        <span>Get in touch</span>
+      </div>
+
+      <div className="sec-eight-form">
+        <div className="sec-eight-form-1">
+          <div className="sec-eight-form-1-inner">
+            <div className="social-link-holder">
+              <div className="social-icon-holder">
+                <PhoneOutlined />
+              </div>
+              <div className="social-link">
+                <div className="social-tag">
+                  <span>Call Me</span>
+                </div>
+                <div className="social-username">+91 7350427767</div>
+              </div>
+            </div>
+            <div className="social-link-holder">
+              <div className="social-icon-holder">
+                <EmailOutlined />
+              </div>
+              <div className="social-link">
+                <div className="social-tag">
+                  <span>Email</span>
+                </div>
+                <div className="social-username">atharva.parkale@gmail.com</div>
+              </div>
+            </div>
+            <div className="social-link-holder">
+              <div className="social-icon-holder">
+                <BsInstagram />
+              </div>
+              <div className="social-link">
+                <div className="social-tag">
+                  <span>Instagram</span>
+                </div>
+                <div className="social-username">_atharvaparkale_167</div>
+              </div>
+            </div>
+            <div className="social-link-holder">
+              <div className="social-icon-holder">
+                <BsLinkedin />
+              </div>
+              <div className="social-link">
+                <div className="social-tag">
+                  <span>LinkedIn</span>
+                </div>
+                <div className="social-username">Atharva Parkale</div>
+              </div>
+            </div>
+            <div className="social-link-holder">
+              <div className="social-icon-holder">
+                <BsGithub />
+              </div>
+              <div className="social-link">
+                <div className="social-tag">
+                  <span>GitHub</span>
+                </div>
+                <div className="social-username">AtharvaParkale</div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div className="sec-eight-form-2">
+          <form
+            className="sec-eight-form-inner"
+            action="https://formspree.io/f/mjvdblkg"
+            method="POST"
+          >
+            <div className="form-inner-sections form-section-1">
+              <div className="form-section-1_1">
+                <span>Name</span>
+                <input type="text" name="Sender" autoComplete="off" required />
+              </div>
+
+              <div className="form-section-1_1">
+                <span>Email</span>
+                <input type="email" name="Email" autoComplete="off" required />
+              </div>
+            </div>
+            <div className="form-inner-sections form-section-2">
+              <div className="form-section-2_1">
+                <span>Project</span>
+                <input type="text" name="Project" />
+              </div>
+            </div>
+            <div className="form-inner-sections form-section-3">
+              <div className="form-section-3_1">
+                <span>Message</span>
+                <textarea name="Message"></textarea>
+              </div>
+            </div>
+            <div className="form-inner-sections form-section-4">
+              <input type="submit" value="Send Message" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SectionEight
+export default SectionEight;
